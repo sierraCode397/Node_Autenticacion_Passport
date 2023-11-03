59fb2d0171b200dbda4ed3d90ef5156c689476cb
izaack107 <izaack107@gmail.com>
2023-11-02 19:39:02 -0600

    FIX 2.0 Creacion de usuario por defecto

--- api/db/migrations/20231004212917-Create-All_tables.js
+++ api/db/migrations/20231004212917-Create-All_tables.js
@@ -18,6 +18,14 @@ const { OrderProductSchema, ORDER_PRODUCT_TABLE } = require('../models/order-pro
 module.exports = {
   async up (queryInterface) {
     await queryInterface.createTable(USER_TABLE, UserSchema);
+    const hash = await bcrypt.hash(config.authPassword, 10);
+    await queryInterface.bulkInsert(USER_TABLE, [
+      {
+        email: config.authCorreo,
+        password: hash,
+        role: 'prime'
+      }
+    ]);
     await queryInterface.createTable(CATEGORIE_TABLE, CategorieSchema);
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
@@ -47,15 +55,6 @@ module.exports = {
       }
     });
     await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
-    const hash = await bcrypt.hash(config.authPassword, 10);
-    await queryInterface.bulkInsert(USER_TABLE, [
-      {
-        email: config.authCorreo,
-        password: hash,
-        role: 'prime',
-        created_at: new Date()
-      }
-    ]);
   },
 
   async down (queryInterface) {
